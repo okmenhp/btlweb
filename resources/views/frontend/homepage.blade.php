8eaed87e116ab20ac59e56c6f488211a7f9703bf
phamduc <phamdinhduchp2017@gmail.com>
2021-10-10 20:59:48 +0700

    ducpc

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -1,10 +1,55 @@
-<?php 
-
 <html>
-    <head>
 
-    </head>
-    <body>
-        
-    </body>
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="bg"></div>
+        <div class="sticky-header">
+            <div class="container custom-container">
+                <div class="header-top-area d-lg-block">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <ul>
+                                <li>FOLLOW</li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6"></div>
+                    </div>
+                </div>
+                <div class="logo">
+                    <img src="{{ asset('assets/images/h6_logo.png') }}" alt="Logo">
+                </div>
+                <div class="header-bottom-area">
+                    <div class="row">
+                        <div class="col-sm-6 pt-4">
+                            <ul class="left">
+                                <li>HOME</li>
+                                <li>ABOUT</li>
+                                <li>OVERVIEW</li>
+                                <li>COMMUNITY</li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 pt-4">
+                            <ul class="right">
+                                <li>PAGES</li>
+                                <li>STORE</li>
+                                <li>BLOG</li>
+                                <li>CONTACT</li>
+                                <li><i class="fa fa-search"></i></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="header-bottom-bg"></div> -->
+    </div>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend/dashboard');
+    return view('frontend/homepage');
 });
 
 Route::get('/admin', function () {
