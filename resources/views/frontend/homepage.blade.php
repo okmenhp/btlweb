1951ab70317f3c08e1e4da64079d86d7c56b08db
phamduc <phamdinhduchp2017@gmail.com>
2021-10-20 00:24:04 +0700

    duc

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -13,10 +13,38 @@
    
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/slick.css') }}"/>
     <script type="text/javascript" src="{{ asset('assets/js/slick.min.js') }}"></script>
-
-
 </head>
 <body>
+    {{-- Navbar-sticky --}}
+    <nav class="navbar sticky-top navbar-hide nav-custom" id="nav-sticky">
+        <div class="container p-4 d-block">
+            <div class="row">
+               <div class="col-md-6 menu-left">
+                    <ul>
+                        <li><a href="" class="active">TRANG CHỦ</a></li>
+                        <li><a href="">GIỚI THIỆU</a></li>
+                        <li><a href="">TIN TỨC</a></li>
+                        <li><a href="">SỰ KIỆN</a></li>
+                    </ul>
+                </div>
+                <div class="logo">
+                    <img src="https://themebeyond.com/html/geco/Geco/img/logo/h6_logo.png" alt="logo" class="logo-center">
+                </div>
+                <div class="col-md-6 menu-right d-flex justify-content-end align-item-center">
+                    <ul>
+                        <li><a href="">VIDEO</a></li>
+                        <li><a href="">HƯỚNG DẪN</a></li>
+                        <li><a href="">HỖ TRỢ</a></li>
+                        <li><a href="">LIÊN LẠC</a></li>
+			            <li>
+                            <div class="btn-search"><a href="javascript:void(0)"><i class="fa fa-search"></i></a></div>
+                        </li>
+                    </ul>
+                </div> 
+            </div>
+                
+        </div>
+    </nav>
     {{-- Header --}}
     <div class="header">
         <div class="container">
@@ -35,7 +63,7 @@
             <div class="logo">
                 <img src="https://themebeyond.com/html/geco/Geco/img/logo/h6_logo.png" alt="logo" class="logo-center">
             </div>
-            <div class="row header-menu">
+            <div class="row header-menu ">
                 <div class="col-md-6 menu-left">
                     <ul>
                         <li><a href="" class="active">TRANG CHỦ</a></li>
@@ -50,12 +78,21 @@
                         <li><a href="">HƯỚNG DẪN</a></li>
                         <li><a href="">HỖ TRỢ</a></li>
                         <li><a href="">LIÊN LẠC</a></li>
-			            <li><a href="javascript:void(0)"><i class="fa fa-search"></i></a></li>
-                        
+			            <li>
+                            <div class="btn-search"><a href="javascript:void(0)"><i class="fa fa-search"></i></a></div>
+                        </li>
                     </ul>
                 </div>
             </div>
         </div>
+        <div class="wrapper">
+            <div class="search-data">
+               <input type="text" required>
+               <div class="line"></div>
+               <label>Type to search..</label>
+               <span class="fa fa-search"></span>
+            </div>
+         </div>
     </div>
     {{-- Banner --}}
     <div class="banner">
@@ -248,7 +285,102 @@
         </div>
     </div>
     {{-- Videos --}}
-
+    <div class="video">
+        <div class="container">
+            <div class="row">
+                <div class="row pb-5">
+                    <div class="col-md-12 trending-news-title">
+                        <h4>Video</h4>
+                    </div>
+                </div>  
+                <div class="row">
+                    <div class="col-md-8 video-left">
+                        <img src="https://themebeyond.com/html/geco/Geco/img/images/stream_thumb01.jpg" alt="">
+                    </div>
+                    <div class="col-md-4 video-right">
+                        <div class="row">
+                            <div class="video-right-card pb-3">
+                            <img src="https://themebeyond.com/html/geco/Geco/img/images/stream_thumb02.jpg" alt="">
+                        </div>
+                        <div class="video-right-card pb-3">
+                            <img src="https://themebeyond.com/html/geco/Geco/img/images/stream_thumb03.jpg" alt="">
+                        </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- Latest News --}}
+    <div class="latest-news">
+        <div class="container">
+            <div class="row">
+                <div class="row pb-5 pt-5">
+                    <div class="col-md-12 trending-news-title">
+                        <h4>Tin tức</h4>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="card-latest-news d-flex">
+                        <img src="https://themebeyond.com/html/geco/Geco/img/blog/lgn_thumb01.jpg" alt="">
+                        <div class="right">
+                            <div class="card-tag">
+                                <a href="">SỰ KIỆN</a>    
+                            </div>
+                            <div class="date">
+                                <span>DECEMBER 19, 2021</span>
+                            </div>
+                            <div class="title">
+                                <a href=""><h1>Director of nation intelligence Trump ignored</h1></a>
+                            </div>
+                            <div class="content">
+                                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-latest-news d-flex">
+                        <img src="https://themebeyond.com/html/geco/Geco/img/blog/lgn_thumb02.jpg" alt="">
+                        <div class="right">
+                            <div class="card-tag">
+                                <a href="">SỰ KIỆN</a>    
+                            </div>
+                            <div class="date">
+                                <span>DECEMBER 19, 2021</span>
+                            </div>
+                            <div class="title">
+                                <a href=""><h1>Director of nation intelligence Trump ignored</h1></a>
+                            </div>
+                            <div class="content">
+                                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-latest-news d-flex">
+                        <img src="https://themebeyond.com/html/geco/Geco/img/blog/lgn_thumb03.jpg" alt="">
+                        <div class="right">
+                            <div class="card-tag">
+                                <a href="">SỰ KIỆN</a>    
+                            </div>
+                            <div class="date">
+                                <span>DECEMBER 19, 2021</span>
+                            </div>
+                            <div class="title">
+                                <a href=""><h1>Director of nation intelligence Trump ignored</h1></a>
+                            </div>
+                            <div class="content">
+                                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+               <div class="col-md-12 d-flex justify-content-center">
+                   <a class="btn-more" href="">Xem thêm</a>
+               </div>
+               
+            </div>
+        </div>
+    </div>
 </body>
 <script>
     $('.carousel').slick({
@@ -288,4 +420,89 @@
     });
 	
 </script>
+<script>
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#nav-sticky').removeClass('navbar-hide');
+        } 
+        else{
+            $('#nav-sticky').addClass('navbar-hide');
+        }
+    });
+
+</script>
+<script>
+    $(".btn-search").click(function(){
+      $(".wrapper").addClass("active");
+    //   $(this).css("display", "none");
+      $(".search-data").fadeIn(500);
+      $(".close-btn").fadeIn(500);
+      $(".search-data .line").addClass("active");
+      setTimeout(function(){
+        $("input").focus();
+        $(".search-data label").fadeIn(500);
+        $(".search-data span").fadeIn(500);
+      }, 800);
+    });
+    
+    $(".wrapper").click(function(){
+      $(".wrapper").removeClass("active");
+      $(".search-btn").fadeIn(800);
+      $(".search-data").fadeOut(500);
+      $(".search-data .line").removeClass("active");
+      $("input").val("");
+      $(".search-data label").fadeOut(500);
+      $(".search-data span").fadeOut(500);
+    });
+ </script>
+ <script>
+     $(".btn-search").click(function(){
+        disableScroll();
+     });
+     $(".wrapper").click(function(){
+        enableScroll();
+     });
+
+    // left: 37, up: 38, right: 39, down: 40,
+    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+    var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+    function preventDefault(e) {
+    e.preventDefault();
+    }
+
+    function preventDefaultForScrollKeys(e) {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    }
+
+    // modern Chrome requires { passive: false } when adding event
+    var supportsPassive = false;
+    try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; } 
+    }));
+    } catch(e) {}
+
+    var wheelOpt = supportsPassive ? { passive: false } : false;
+    var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+    // call this to Disable
+    function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+    }
+
+    // call this to Enable
+    function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+    }
+ </script>
 </html>
