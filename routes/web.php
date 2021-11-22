a5d79170ccdd804829660df0b636f7e16542b8c4
phamduc <phamdinhduchp2017@gmail.com>
2021-11-22 23:53:15 +0700

    duc

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'Frontend\DashboardController@index');
 
 Route::get('/', function () {
-    return view('frontend/news');
+    return view('frontend/overview');
 });
 
 Route::get('/videos', function () {
